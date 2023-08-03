2d91b380ddfd4096edaf23dff00022e6ddeefa62
RjCoyle20 <rjcoyle20@gmail.com>
2023-08-02 21:40:47 -0500

    Finished Part 1 of Part 1

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Need to Reads!";
+   let book1 = "https://i.pinimg.com/originals/52/45/d6/5245d6437deb06f988f41cd9328a790b.jpg";
+   let book2 = "https://www.psupress.org/images/covers/294wide/978-0-271-08231-8md_294.jpg";
+   let book3 = "https://hannahdoublebarrel.files.wordpress.com/2013/03/picture-of-dorian.jpg";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Ugly 90s Version of The Hobbit" />
+         <img src={book2} alt="Don Quixote of La Mancha" />
+         <img src={book3} alt="The Picture of Dorian Gray" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,14 @@
+import classes from './ChoresList.module.css'
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   const chores = ["Do the dishes", "Take dogs for walk"];
+   return (<div>
+         <h3 className = {classes.choresHeading}> 
+            To-Do List
+         </h3>
+         <ol>
+         <li className={classes.choresText}>{chores[0]}</li>
+          <li className={classes.choresText}>{chores[1]}</li>
+      </ol>
+      </div>
+   )
 }
